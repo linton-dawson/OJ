0d157998df09715cef4739faadea513f87b0c1b8
linton-dawson <pushkarkukde@gmail.com>
2018-11-01 23:32:04 +0530

    Adding words.c

---
+++ Codeforces/words.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int t;
+scanf("%d",&t);
+while(t--){
+char str[100];
+int len;
+scanf("%s",str);
+if(strlen(str)>10)
+printf("%c%d%c\n",str[0],strlen(str)-2,str[strlen(str)-1]);
+else
+printf("%s\n",str);
+}
+return 0;
+}
